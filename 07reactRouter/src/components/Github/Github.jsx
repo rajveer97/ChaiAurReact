2dee4745f0163268f06540a509414d321c273395
Rajveer Pandey <kram35477@gmail.com>
2024-10-23 23:16:32 +0530

    fecting data from api is optimized by using useLoaderData function of react-router-dom

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,17 +1,19 @@
 // import React from 'react'
-
-import { useEffect, useState } from "react"
+import { useLoaderData } from "react-router-dom"
+// import { useEffect, useState } from "react"
 
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/rajveer97')
-        .then(response => response.json())
-        .then(data => {
-            console.log("zzz data",data);
-            setData(data)
-        })
-    }, [])
+  const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/rajveer97')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log("zzz data",data);
+    //         setData(data)
+    //     })
+    // }, [])
+
   return (
     <div className="text-center bg-gray-600 text-white m-4 p-4 text-3xl">Github followers: {data.followers}
     <img src={data.avatar_url} alt="Git picture" width={200} />
@@ -20,3 +22,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/rajveer97')
+  return response.json()
+}
